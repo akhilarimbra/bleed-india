668e06cdcdf2787c53a0af02bed96801df294f06
Akhil Raj <nilamburakhil@gmail.com>
2016-02-29 21:51:31 +0530

    Completed UI

---
+++ Bleed India/ContactViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ContactViewController.swift
+//  Bleed India
+//
+//  Created by Akhil Raj on 2016-02-27.
+//  Copyright © 2016 Cyboz. All rights reserved.
+//
+
+import UIKit
+
+class ContactViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.title = "Contact"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
